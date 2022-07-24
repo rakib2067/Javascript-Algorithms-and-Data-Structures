fa5dc661ff076dba9f1518c367515b161a99bcbf
rakib2067 <rakib7@hotmail.co.uk>
2022-07-24 18:29:57 +0100

    linkedlist get

--- DoublyLinkedList/index.js
+++ DoublyLinkedList/index.js
@@ -71,6 +71,27 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    let mid = this.length / 2;
+    if (index <= mid) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
 }
 
 let list = new DoublyLinkedList();
