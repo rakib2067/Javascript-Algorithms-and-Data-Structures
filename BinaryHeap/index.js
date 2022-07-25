3b3dbb6c9e668eed87cf9ecbeb70253326c835ee
rakib2067 <rakib7@hotmail.co.uk>
2022-07-25 14:48:18 +0100

    binary heap tbd

--- BinaryHeap/index.js
+++ BinaryHeap/index.js
@@ -6,6 +6,24 @@ class BinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+    return this.values;
+  }
+  bubbleUp() {
+    let index = this.values.length - 1;
+    let element = this.values[index];
+    while (true) {
+      let parentIdx = Math.floor(index - 1 / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[index] = parent;
+      index = parent;
+    }
+  }
 }
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
