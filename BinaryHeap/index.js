6db61ab3874c8ebd3d1f86220c2287fad0312f75
rakib2067 <rakib7@hotmail.co.uk>
2022-07-25 19:24:19 +0100

    BHeap extractMax and sinkdown mostly passing

--- BinaryHeap/index.js
+++ BinaryHeap/index.js
@@ -6,6 +6,65 @@ class BinaryHeap {
   constructor() {
     this.values = [];
   }
+
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+    return this.values;
+  }
+  bubbleUp() {
+    let currentIdx = this.values.length - 1;
+    let currentElement = this.values[currentIdx];
+    while (currentIdx > 0) {
+      let parentIdx = Math.floor((currentIdx - 1) / 2);
+      let parentElement = this.values[parentIdx];
+      if (currentElement <= parentElement) break;
+
+      this.values[parentIdx] = currentElement;
+      this.values[currentIdx] = parentElement;
+
+      currentIdx = parentIdx;
+    }
+  }
+  extractMax() {
+    let max = this.values[0];
+    let end = this.values.pop();
+    this.values[0] = end;
+
+    this.sinkDown();
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    let element = this.values[0];
+
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap == null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
